c00e71cfdc381c09e5464eb4963312320371f677
Faustina Puspamurti <55085124+FaustinaPuspamurti@users.noreply.github.com>
2020-08-31 07:37:39 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using beratIdeal_15_FaustinaPuspamurti;
+using System;
+
+namespace hitungBerat_15_FaustinaPuspamurti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            beratPriaWanita beratw = new beratPriaWanita();
+            Console.Write("Masukan Tinggi Badanmu (Wanita) :");
+            double a = Convert.ToDouble(Console.ReadLine());
+            beratw.beratIdealWanita(a);
+            Console.ReadLine();
+
+            beratPriaWanita beratp = new beratPriaWanita();
+            Console.Write("Masukan Tinggi Badanmu (Pria) :");
+            double b = Convert.ToDouble(Console.ReadLine());
+            beratp.beratIdealPria(b);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ beratIdealPriaWanita.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace beratIdeal_15_FaustinaPuspamurti
+{
+    public class beratPriaWanita
+    {
+        public double beratIdealWanita(double tb)
+        {
+            double hasil = (tb - 100) - (tb - 100) * 0.15;
+            Console.WriteLine("Berat Ideal Mu (Untuk Wanita) :" + hasil);
+            return hasil;
+        }
+        public double beratIdealPria(double tb)
+        {
+            double hasil = (tb - 100) - (tb - 100) * 0.10;
+            Console.WriteLine("Berat Ideal Mu (Untuk Pria) :" + hasil);
+            return hasil;
+        }
+
+    }
+}
